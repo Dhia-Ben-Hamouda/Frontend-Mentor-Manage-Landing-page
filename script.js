53d4a0d3f170b41c5632d0efb85e002c86f4e92b
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2023-04-24 11:27:47 +0100

    working on review slider

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ const swiper = new Swiper('.swiper', {
 
     direction: 'horizontal',
     loop: true,
-    slidesPerView:3
+    slidesPerView:3,
+    spaceBetween:24,
+    autoplay: {
+      delay:3000,
+      disableOnInteraction: false,
+    },
+    grabCursor: true,
   });
