e9b08bf318bc41f9113f019e9895444a1ef4f717
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2023-05-13 18:43:37 +0100

    fix background image

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const swiper = new Swiper('.swiper', {
 
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView:3,
-    spaceBetween:24,
-    autoplay: {
-      delay:3000,
-      disableOnInteraction: false,
-    },
-    grabCursor: true,
-  });
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: window.innerWidth > 680 ? 3 : 1,
+	spaceBetween: 24,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	grabCursor: true,
+});
+
+const bars = document.querySelector(".bars");
+
+bars.addEventListener("click", (e) => {
+	bars.classList.toggle("clicked");
+})
